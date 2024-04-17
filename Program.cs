4569e68a700711032196dd13202a1abb080ce155
Gui-gt <30755guilherme@gmail.com>
2024-04-16 22:41:55 -0300

    att

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CURSO_CSHARP
+{
+    public class Program
+    {
+        static void Main ()
+        {
+            int num1,num2, res;
+            num1=1;
+            num2=1;
+            res = num1 + num2;
+            Console.Write(res);
+        }
+    }
+}
---
+++ aula38/aula38.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace CURSO_CSHARP
+{
+    public class aula38
+    {
+        static void Main ()
+        {
+            int num1,num2, res;
+            num1=1;
+            num2=1;
+            res = num1 + num2;
+            Console.Write(res);
+        }
+    }
+}
